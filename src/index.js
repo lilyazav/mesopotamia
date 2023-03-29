66687ce95f1159a36cef55aa553dd9f66eba5ea9
lilyazav <lillianzavatsky@gmail.com>
2023-03-29 13:38:48 -0400

    Fixed moving in last row, added rosettes + rosette logic, added no 2 on same piece logic, some formatting

--- src/index.js
+++ src/index.js
@@ -9,13 +9,9 @@ import {ReactComponent as DiceNo} from './svg/dice_no.svg'
 
 class RoyalGameOfUr extends React.Component {
 // functionality to add: 
-// 1. unselect tile move 
-// 2. tile can't go where own piece is
-// 3. tile can't take another on rosette
-// 4. next player if you don't have any moves
-// 5. can't actually go home. lol. 
+// 1. next player if you don't have any moves - forfeit turn button? but checks if you have moves
+// 2. can't actually go home. lol. 
 
-// not working on player 1 when on middle row, second to last
 
     constructor(props){
         super(props);
@@ -26,16 +22,68 @@ class RoyalGameOfUr extends React.Component {
             turn: 1, 
             tileNum: null,
             roll: null,
+            rollNeeded: true,
+            tip: null, 
+            message: null, 
             error: null
         }
     }
 
-
     rosettes = [0, 2, 10, 18, 20];
     nullTiles = [6, 8, 9, 11];
 
+    diceRoll() {
+        if(this.state.rollNeeded){
+            let roll= Math.floor(Math.random() * 5)
+
+            if(roll === 0){
+                let turn = this.state.turn
+                return this.setState({ roll: roll, message: "You rolled a 0. Next player's turn." }, 
+                    () => {
+                        this.nextTurn(turn === 1 ? 2 : 1)
+                    })}
+
+            if(this.state.error === "Roll the dice first!"){
+                this.setState({ error: null, roll: roll})}
+
+            this.setState({ roll: roll, rollNeeded: false, message: null  })
+        }
+    }
+
+    diceDisplay(){
+        const roll = this.state.roll;
+        if(roll === 0){
+            return (
+                <div className="clickable opaque">
+                    <DiceNo /> 
+                    <DiceNo /> 
+                    <DiceNo /> 
+                    <DiceNo /> 
+                </div>
+            )
+        } else if (roll !== null){
+            let diceArray = []
+            for(let i = roll; i > 0; i--){
+                diceArray.push(<DiceYes />)
+            }
+            for(let i = 4 - roll; i > 0; i--){
+                diceArray.push(<DiceNo />)
+            }
+            return diceArray;
+        } else {
+            return(
+            <div className="opaque clickable">
+                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
+                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
+                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
+                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
+            </div>
+            )
+        }
+    }
+
     nextTurn(turn){
-        this.setState({ roll: null, error: null, red: null, turn: turn, tileNum: null})
+        this.setState({ error: null, rollNeeded: true, red: null, turn: turn, tileNum: null })
     }
         
     // where a tile goes if it moves 1 space
@@ -49,7 +97,7 @@ class RoyalGameOfUr extends React.Component {
         } else if (rem === 0) {
             if( i === 0){
                 return 1;
-            } else if (i === 18){
+            } else if (i === 12){
                 return 101;
             } else {
                 return i - 3;
@@ -67,7 +115,7 @@ class RoyalGameOfUr extends React.Component {
             } else if (i === 101 ){
                 return false;
             } 
-            else if (i === 20){
+            else if (i === 14){
                 return 101;
             } else {
                 return i - 3;
@@ -87,18 +135,31 @@ class RoyalGameOfUr extends React.Component {
 
     resultOfTileMove(tileNum, roll, player){
         let potentialResult = this.tileMove(tileNum, roll, player);
-        this.setState({ red: potentialResult, tileNum: tileNum})
+        let playerArray = this.state["player".concat(player)];
+        let otherPlayer = player === 1 ? 2 : 1;
+        let otherPlayerArray = this.state["player".concat(otherPlayer)]
+
+
+        if(playerArray.includes(potentialResult)){
+            this.setState({ tip: "You cannot put two of your own tiles on the same spot."})
+        } else if ( this.rosettes.includes(potentialResult) && otherPlayerArray.includes(potentialResult)) {
+            this.setState({ tip: "You cannot take a piece that is on a rosette"})
+        } else {
+            this.setState({ red: potentialResult, tileNum: tileNum, 
+            tip: "Press on null squares to clear potential tile move."})
+        }
+       
     }
 
     checkMoves(i) {
-        let { roll, turn } = this.state
-        if( roll !== null){
+        let { roll, turn, rollNeeded } = this.state
+        if( rollNeeded ) {
+            this.setState({error: "Roll the dice first!"})
+        } else {
             let playerArray = this.state["player".concat(turn)];
             if(playerArray.includes(i)){
                 this.resultOfTileMove(i, roll, turn)
             } 
-        } else {
-            this.setState({error: "Roll the dice first!"})
         }
         
     }
@@ -112,40 +173,51 @@ class RoyalGameOfUr extends React.Component {
 
         let indexChange = playerArray.findIndex((i) => i === tileNum)
         playerArray[indexChange] = red;
-
         // are we kicking out the other player's piece
         let kickOut = otherPlayerArray.findIndex((i) => i === red)
-        if(kickOut !== -1) {
-            let home = otherPlayer === 1 ? 6: 8;
-            otherPlayerArray[kickOut] = home;
-        }
+        let onRosette = this.rosettes.includes(red);
 
-        let newState = {}
+        let newState = { tip: null }
         newState["player".concat(turn)] = playerArray;
         newState["player".concat(otherPlayer)] = otherPlayerArray;
 
-        // additional move for player?
-        let isAdditionalMove = this.rosettes.includes(red);
-        let nextPlayer = isAdditionalMove ? turn : otherPlayer;
-        
-        this.setState(newState, () => this.nextTurn(nextPlayer))
-        
+        if(kickOut !== -1 ){
+            let home = otherPlayer === 1 ? 6: 8;
+            otherPlayerArray[kickOut] = home;
+            this.setState(newState, () => this.nextTurn(otherPlayer))
+        } else {
+            if(onRosette) { newState.tip = "Landing on a rosette gives you an additional move."}
+            let nextPlayer = onRosette ? turn : otherPlayer;
+            this.setState(newState, () => this.nextTurn(nextPlayer))
+        }        
     }
 
     renderSquare(i) {
         const {player1, player2, red } = this.state
         const isPotentialMove = red === i 
+        const isRosette = this.rosettes.includes(i)
+
+        const className  = ()  => {
+            if(isPotentialMove && isRosette) {
+                return "red rosette"
+            } else if (isPotentialMove) {
+                return "red"
+            } else if (isRosette) {
+                return "rosette"
+            } 
+        } 
     
-        return ( <td id={i} onClick={ isPotentialMove ? ()=> this.move() : () => this.checkMoves(i)} 
-        className = {red === i ? "red" : null}>
-            {player1.includes(i)? <Piece1 className = "clickable"/> : null}
-            {player2.includes(i)?  <Piece2 className = "clickable"/> : null}
+        return ( <td id={i} onClick={ isPotentialMove ? () => this.move() : () => this.checkMoves(i)} 
+        className = { className()}
+        >
+            {player1.includes(i)? <div className="container"><Piece1 className = "clickable"/></div> : null}
+            {player2.includes(i)? <div className="container"><Piece2 className = "clickable"/></div> : null}
 
         </td>)
     }
 
     renderNullSquare(i) {
-        return ( <td className="null" id={i}></td>)
+        return ( <td className="null" id={i} onClick={() => this.setState({ red: null, tip: null })}></td>)
     }
 
     renderHome(player) {
@@ -178,82 +250,41 @@ class RoyalGameOfUr extends React.Component {
         return tblList;
     }
 
-    diceRoll() {
-
-        if(this.state.roll === null){
-            let roll= Math.floor(Math.random() * 5)
-
-            if(roll === 0){
-                let turn = this.state.turn
-                // need a message or something for roll = 0
-                return this.nextTurn(turn === 1 ? 2 : 1)
-            }
-
-            if(this.state.error === "Roll the dice first!"){
-                this.setState({ error: null, roll: roll})
-            }
-
-            this.setState({ roll: roll })
-        } else {
-            this.setState({ error: "You have to go first!"})
-        }
-
-    }
-
-    diceDisplay(){
-        const roll = this.state.roll;
-        console.log(roll)
-        if(roll){
-            let diceArray = []
-            for(let i = roll; i > 0; i--){
-                diceArray.push(<DiceYes />)
-            }
-            for(let i = 4 - roll; i > 0; i--){
-                diceArray.push(<DiceNo />)
-            }
-            return diceArray;
-        } else {
-            return(
-            <div className="opaque">
-                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
-                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
-                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
-                { Math.round(Math.random()) === 1 ? <DiceYes /> : <DiceNo />}
-            </div>
-            )
-        }
-    }
-
     render() {
+        const { error, message, turn, rollNeeded, tip } = this.state
         return (
             <div className = "game">
+                { tip ? <div className="tip"> {tip}</div>  : null } 
                 <div className="accessory">
-                    <div className="playerTurn">Player {this.state.turn}, it's your turn.  
-                        {this.state.roll ? " Please make a move." : " Please roll the dice" }
+                    <div className="playerTurn">
+                        { message ? <> {message} <br /></> : null }
+                         Player {turn}, it's your turn.  
+                        {rollNeeded ? " Please roll the dice. " : " Please make a move. " }
                     </div>
-                    <div className="dice" onClick={() => this.diceRoll()}>
+                    { error ? <div className="error"> { error } </div> : null }
+                    <div className={`dice ${rollNeeded ? "clickable" : ""}`}
+                        onClick={ rollNeeded ? () => this.diceRoll() : null}>
                         {this.diceDisplay()}
+                    </div>                       
+                </div>
+                <div className="main">
+                    <div className="player1" onClick={() => this.checkMoves(6)}>
+                        <div>
+                            {this.renderHome("player1")}
+                        </div>
                     </div>
-
-                    {/* <div className="error">{ this.state.error ? this.state.error : null }</div> */}
-                </div>
-                    
-
-
-                
-                {/* <div className="player1" onClick={() => this.checkMoves(6)}>
-                    {this.renderHome("player1")}
-                </div>
-                <table id="board">
-                    <thead />
-                    <tbody>
-                    {this.renderTable()}
-                    </tbody>
-                </table>
-                <div className="player2" onClick={() => this.checkMoves(8)}>
-                    {this.renderHome("player2")}
-                </div> */}
-           
+                    <div className="board">
+                        <table id="board">
+                            <thead />
+                            <tbody>
+                            {this.renderTable()}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="player2" onClick={() => this.checkMoves(8)}>
+                        {this.renderHome("player2")}
+                    </div>
+                </div>           
             </div>
         )
     }
